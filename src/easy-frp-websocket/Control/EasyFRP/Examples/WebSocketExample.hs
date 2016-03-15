c35cef78432ca36f2af6c9791087b2850402eac3
kelemzol <kelemzol@elte.hu>
2016-03-15 22:39:23 +0100

    .

--- src/easy-frp-websocket/Control/EasyFRP/Examples/WebSocketExample.hs
+++ src/easy-frp-websocket/Control/EasyFRP/Examples/WebSocketExample.hs
@@ -3,21 +3,4 @@ module Control.EasyFRP.Examples.WebSocket where
 
 import Control.EasyFRP
 import Control.EasyFRP.WebSocket
-import Network.WebSockets
-
-
-newtype WSConn = WSConn { getPendingConnection :: PendingConnection }
-
-newtype WsMessage = WsMessage { getMessage :: Message }
-
-
-wsGetWSConn :: (MonadIO m) => Int -> FRPT m (Signal WSConn)
-wsGetWSConn = undefined
-
-wsServer :: (MonadIO m) => Int -> (WsMessage -> WsMessage) -> FRPT m (Signal (WsConn, WsMessage), Signal WSConn)
- wsServer= undefined
-
-wsConnServe :: (MonadIO m) => WSConn -> (WsMessage -> WsMessage) -> FRPT m (Signal WsMessage)
-wsConnServe = undefined
-
 
