116c9e332f058c6e57d5e9bcb48ebe8545c4c5d5
Zoltán Kelemen <zoltan.z.kelemen@ericsson.com>
2016-03-17 15:17:42 +0100

    .

--- src/easy-frp-websocket/Control/EasyFRP/WebSocket.hs
+++ src/easy-frp-websocket/Control/EasyFRP/WebSocket.hs
@@ -1,7 +1,7 @@
 
 module Control.EasyFRP.WebSocket
   ( WsConn
-  , wsGetWSConn
+  , wsWaitForRequest
   , wsServer
   , wsConnServe
   ) where
@@ -12,7 +12,15 @@ import Network.WebSockets
 import Control.Monad.IO.Class
 import Data.Aeson
 
-newtype WsConn = WsConn { getPendingConnection :: PendingConnection }
+data WsConn
+  = WsConn
+    { pendingConnection :: PendingConnection
+    , requestHead       :: RequestHead
+    , connection        :: Connection
+    }
+
+wsConn :: PendingConnection -> IO WsConn
+wsConn pc = WsConn pc (pendingRequest pc) <$> acceptRequest pc
 
 newtype WsMessage = WsMessage { getMessage :: Message }
   deriving (Eq)
@@ -20,23 +28,41 @@ newtype WsMessage = WsMessage { getMessage :: Message }
 data Error = DataConversionError String
   deriving (Eq, Show)
 
-wsGetWSConn :: (MonadIO m) => Int -> FRPT m (Signal WsConn)
-wsGetWSConn port = do
+wsWaitForRequest :: (MonadIO m) => Int -> FRPT m (Signal WsConn)
+wsWaitForRequest port = do
     (chan, sig) <- node
-    frpFork $ runServer "localhost" port $ writeChan chan . WsConn
+    frpFork $ runServer "localhost" port $ (writeChan chan =<<) . wsConn
     return sig
 
-wsMessages :: (MonadIO m, ToJSON a, FromJSON a) => WsConn -> FRPT m (Signal a)
-wsMessages = undefined
+wsConnServe :: (MonadIO m, ToJSON a, FromJSON a) => WsConn -> (a -> IO a) -> FRPT m (Signal a, Signal Error)
+wsConnServe conn res = do
+    (chanA, sigA) <- node
+    (chanE, sigE) <- node
+    forkLoop $ do
+        mess <- receive (connection conn)
+        case mess of
+            ControlMessage (Close _ _) -> return ()
+            ControlMessage (Pong _)    -> return ()
+            ControlMessage (Ping msg)  -> send (connection conn) (ControlMessage (Pong msg))
+            DataMessage (Binary msg)   -> writeChan chanE (DataConversionError $ "Binary message on\n" ++ show (requestHead conn))
+            DataMessage (Text msg)     -> case eitherDecode msg of
+                Left errorMsg -> writeChan chanE (DataConversionError $ errorMsg ++ " on\n" ++ show (requestHead conn))
+                Right item -> do
+                    writeChan chanA item
+                    item' <- res item
+                    send (connection conn) (DataMessage (Text (encode item')))
+    return (sigA, sigE)
 
-wsConnServeInternal :: (MonadIO m) => WsConn -> (WsMessage -> m WsMessage) -> FRPT m (Signal WsMessage)
-wsConnServeInternal = undefined
+wsConnServeS :: (MonadIO m, ToJSON a, FromJSON a) => Signal WsConn -> (a -> IO a) -> FRPT m (Signal a, Signal Error)
+wsConnServeS sigC res = do
+    (chanA, sigA) <- node
+    (chanE, sigE) <- node
+    chanC <- subsribe sigC
+    forkLoop $ do
+        newConn <- readChan chanC
+        error "TODO: splitting monad content"
 
-wsServerInternal :: (MonadIO m) => Int -> (WsMessage -> m WsMessage) -> FRPT m (Signal (WsConn, WsMessage), Signal WsConn)
-wsServerInternal = undefined
 
-wsConnServe :: (MonadIO m, ToJSON a, FromJSON a) => WsConn -> (a -> m a) -> FRPT m (Signal a, Signal Error)
-wsConnServe = undefined
 
 wsServer :: (MonadIO m, ToJSON a, FromJSON a) => Int -> (a -> m a) -> FRPT m (Signal (WsConn, a), Signal (WsConn, Error), Signal WsConn)
 wsServer = undefined
